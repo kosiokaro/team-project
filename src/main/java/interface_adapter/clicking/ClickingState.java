44f13665d221188ec6b6ae7ccbc55fa1865c355d
Eileen1222 <yihe.xiao@mail.utoronto.ca>
2025-12-02 14:41:59 -0500

    did CheckStyle for some files

--- src/main/java/interface_adapter/clicking/ClickingState.java
+++ src/main/java/interface_adapter/clicking/ClickingState.java
@@ -17,21 +17,27 @@ public class ClickingState {
     public String getTitle() {
         return title;
     }
+
     public String getOverview() {
         return overview;
     }
+
     public String getLanguage() {
         return language;
     }
+
     public String getPosterUrl() {
         return posterUrl;
     }
+
     public int getYear() {
         return year;
     }
+
     public double getRating() {
         return rating;
     }
+
     public List<String> getGenres() {
         return genres;
     }
@@ -39,27 +45,33 @@ public class ClickingState {
     public void setTitle(String title) {
         this.title = title;
     }
+
     public void setOverview(String overview) {
         this.overview = overview;
     }
+
     public void setLanguage(String language) {
         this.language = language;
     }
+
     public void setPosterUrl(String posterUrl) {
         this.posterUrl = posterUrl;
     }
+
     public void setYear(int year) {
         this.year = year;
     }
+
     public void setRating(double rating) {
         this.rating = rating;
     }
+
     public void setGenres(List<String> genres) {
         this.genres = genres;
     }
 
-    public void setErrorMessage(String s) {
-        this.errorMessage = s;
+    public void setErrorMessage(String str) {
+        this.errorMessage = str;
     }
 
     public String getErrorMessage() {
